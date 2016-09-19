f87b7a46fa93df096aae186fed5e7207b6b5a83d
andr-pash <andreas.pasha@googlemail.com>
2016-09-19 20:15:55 +0200

    added smooth scroll and fixed btn__book

--- js/main.js
+++ js/main.js
@@ -76,6 +76,21 @@ $('document').ready(function () {
         $('.nav-menu__pointer').css('top', newPos);
     }
 
+    $(function () {
+        $('a[href*="#"]:not([href="#"])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 700);
+                    return false;
+                }
+            }
+        });
+    });
+
     // APPEARANCE BOOKING BUTTON ON SCROLL
     $(window).scroll(function () {
         var offSet = window.pageYOffset;
